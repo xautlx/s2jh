58256c2049f91cc3b46925584892678d37b697cf
xautlx <xautlx@hotmail.com>
2013-12-12 13:03:26 +0800

    新增s2:hidden标签
    Signed-off-by: xautlx <xautlx@hotmail.com>

--- core-service/src/main/java/org/apache/struts2/views/jsp/ui/S2HiddenTag.java
+++ core-service/src/main/java/org/apache/struts2/views/jsp/ui/S2HiddenTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TextFieldTag.java 1389534 2012-09-24 19:43:44Z jogep $
+ * $Id: HiddenTag.java 1389534 2012-09-24 19:43:44Z jogep $
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -24,14 +24,12 @@ package org.apache.struts2.views.jsp.ui;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.struts2.components.UIBean;
 
 /**
- * 扩展标准的textfield标签，提供自动化数据校验处理支持
- * 及其他如样式等默认值定义
+ * 扩展标准的hidden标签，提供自动化数据校验处理支持
  */
-public class S2TextFieldTag extends TextFieldTag {
+public class S2HiddenTag extends HiddenTag {
 
     /** 
      * 如果在元素中未定义此属性，则按照属性的类型、JSR303 Validator注解、Hibernate Entity注解等自动组合生成JQuery Validator校验语法字符串
@@ -39,62 +37,21 @@ public class S2TextFieldTag extends TextFieldTag {
      */
     protected String validator;
 
-    /** 隐藏hidden元素接收id属性值 */
-    protected String hiddenName;
-    /** 隐藏hidden元素接收value属性值 */
-    protected String hiddenValue;
-    /** 隐藏hidden元素接收class属性值 */
-    protected String hiddenCssClass;
-
     protected void populateParams() {
-        if (StringUtils.isNotBlank(hiddenName)) {
-            dynamicAttributes.put("hiddenName", hiddenName);
-            
-            Object realHiddenValue = null;
-            if (this.hiddenValue != null) {
-                realHiddenValue = findValue(this.hiddenValue);
-            } else {
-                realHiddenValue = findValue(this.hiddenName);
-            }
-            if (realHiddenValue == null) {
-                realHiddenValue = "";
-            }
-            dynamicAttributes.put("hiddenValue", realHiddenValue);
-            
-            if (StringUtils.isNotBlank(hiddenCssClass)) {
-                dynamicAttributes.put("hiddenCssClass", hiddenCssClass);
-            }
-        }
-        
+
         super.populateParams();
         UIBean uiBean = ((UIBean) component);
         TagValidatorAttributeBuilder.buildValidatorAttribute(validator, this, this.getStack(),
                 (HttpServletRequest) this.pageContext.getRequest(), uiBean);
-        if (this.cssClass == null) {
-            uiBean.setCssClass("input-fluid");
-        }
         if (this.theme == null) {
             uiBean.setTheme("bootstrap");
         }
         if (this.id == null) {
-            uiBean.setId("text_" + RandomStringUtils.randomAlphabetic(10));
+            uiBean.setId("hide_" + RandomStringUtils.randomAlphabetic(10));
         }
-
     }
 
     public void setValidator(String validator) {
         this.validator = validator;
-    }
-
-    public void setHiddenName(String hiddenName) {
-        this.hiddenName = hiddenName;
-    }
-
-    public void setHiddenValue(String hiddenValue) {
-        this.hiddenValue = hiddenValue;
-    }
-
-    public void setHiddenCssClass(String hiddenCssClass) {
-        this.hiddenCssClass = hiddenCssClass;
     }
 }
