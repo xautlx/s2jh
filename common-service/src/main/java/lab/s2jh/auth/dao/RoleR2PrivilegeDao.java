092a5485471745290ad57bf51e84518305f16b86
xautlx <xautlx@hotmail.com>
2014-01-03 17:58:09 +0800

    修正权限列表遗漏权限的排序属性控制
    Signed-off-by: xautlx <xautlx@hotmail.com>

--- common-service/src/main/java/lab/s2jh/auth/dao/RoleR2PrivilegeDao.java
+++ common-service/src/main/java/lab/s2jh/auth/dao/RoleR2PrivilegeDao.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleR2PrivilegeDao extends BaseDao<RoleR2Privilege, String> {
 
-    @Query("select r2 from RoleR2Privilege r2  inner join r2.privilege as p  inner join r2.role r where p.disabled=false and r.code != :excludeRoleCode")
+    @Query("select r2 from RoleR2Privilege r2  inner join r2.privilege as p  inner join r2.role r where p.disabled=false and r.code != :excludeRoleCode order by p.orderRank desc")
     @QueryHints({ @QueryHint(name = org.hibernate.ejb.QueryHints.HINT_CACHEABLE, value = "true") })
     List<RoleR2Privilege> findEnabledExcludeRole(@Param("excludeRoleCode") String excludeRoleCode);
 
