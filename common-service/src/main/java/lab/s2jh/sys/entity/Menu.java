f2047e40f824c580e8be87c97efe67f284ca9a8c
xautlx <xautlx@hotmail.com>
2014-02-25 09:40:57 +0800

    menu对象属性编辑方式调整
    Signed-off-by: xautlx <xautlx@hotmail.com>

--- common-service/src/main/java/lab/s2jh/sys/entity/Menu.java
+++ common-service/src/main/java/lab/s2jh/sys/entity/Menu.java
@@ -20,7 +20,6 @@ import javax.persistence.UniqueConstraint;
 import lab.s2jh.core.annotation.MetaData;
 import lab.s2jh.core.entity.BaseEntity;
 import lab.s2jh.core.entity.annotation.EntityAutoCode;
-import lab.s2jh.core.web.json.EntityIdSerializer;
 
 import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.hibernate.annotations.Cache;
@@ -28,7 +27,6 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Entity
 @Table(name = "tbl_SYS_MENU", uniqueConstraints = @UniqueConstraint(columnNames = { "title", "PARENT_ID" }))
@@ -119,7 +117,7 @@ public class Menu extends BaseEntity<String> implements Comparable<Menu> {
 
     @ManyToOne(cascade = CascadeType.DETACH)
     @JoinColumn(name = "PARENT_ID")
-    @JsonSerialize(using = EntityIdSerializer.class)
+    @JsonIgnore
     public Menu getParent() {
         return parent;
     }
--- common-service/src/main/java/lab/s2jh/sys/web/action/MenuController.java
+++ common-service/src/main/java/lab/s2jh/sys/web/action/MenuController.java
@@ -1,5 +1,8 @@
 package lab.s2jh.sys.web.action;
 
+import java.util.List;
+import java.util.Map;
+
 import lab.s2jh.core.annotation.MetaData;
 import lab.s2jh.core.pagination.GroupPropertyFilter;
 import lab.s2jh.core.pagination.PropertyFilter;
@@ -9,8 +12,11 @@ import lab.s2jh.core.web.BaseController;
 import lab.s2jh.sys.entity.Menu;
 import lab.s2jh.sys.service.MenuService;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.struts2.rest.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.common.collect.Maps;
 
 public class MenuController extends BaseController<Menu, String> {
 
@@ -51,5 +57,29 @@ public class MenuController extends BaseController<Menu, String> {
     @MetaData(value = "删除")
     public HttpHeaders doDelete() {
         return super.doDelete();
+    }
+
+    @MetaData(value = "树形表格数据")
+    public HttpHeaders treeGridData() {
+        Map<String, Menu> menuDatas = Maps.newLinkedHashMap();
+        loopTreeGridData(menuDatas, menuService.findRoots());
+        setModel(menuDatas.values());
+        return buildDefaultHttpHeaders();
+    }
+
+    private void loopTreeGridData(Map<String, Menu> menuDatas, List<Menu> menus) {
+        for (Menu menu : menus) {
+            Menu parent = menu.getParent();
+            List<Menu> children = menuService.findChildren(menu);
+            menu.addExtraAttribute("level", menu.getLevel());
+            menu.addExtraAttribute("parent", parent == null ? "" : parent.getId());
+            menu.addExtraAttribute("isLeaf", CollectionUtils.isEmpty(children) ? true : false);
+            menu.addExtraAttribute("expanded", true);
+            menu.addExtraAttribute("loaded", true);
+            menuDatas.put(menu.getId(), menu);
+            if (!CollectionUtils.isEmpty(children)) {
+                loopTreeGridData(menuDatas, children);
+            }
+        }
     }
 }
