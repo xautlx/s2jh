376078eacb9effbdb491e006ed8ffb5c68c4b4ef
xautlx <xautlx@hotmail.com>
2014-03-20 14:12:39 +0800

    为日期帮助类添加功能方法
    Signed-off-by: xautlx <xautlx@hotmail.com>

--- core-service/src/main/java/lab/s2jh/core/util/DateUtils.java
+++ core-service/src/main/java/lab/s2jh/core/util/DateUtils.java
@@ -1,50 +1,100 @@
 package lab.s2jh.core.util;
 
+import java.text.DateFormat;
 import java.text.DecimalFormat;
-import java.text.Format;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.joda.time.DateTime;
+
 public class DateUtils {
 
-	public final static String DEFAULT_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public final static String DEFAULT_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-	public final static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    public final static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
 
-	public final static Format DEFAULT_TIME_FORMATER = new SimpleDateFormat(DEFAULT_TIME_FORMAT);
+    public final static DateFormat DEFAULT_TIME_FORMATER = new SimpleDateFormat(DEFAULT_TIME_FORMAT);
 
-	public final static Format DEFAULT_DATE_FORMATER = new SimpleDateFormat(DEFAULT_DATE_FORMAT);
+    public final static DateFormat DEFAULT_DATE_FORMATER = new SimpleDateFormat(DEFAULT_DATE_FORMAT);
 
-	public static String formatDate(Date date) {
-		if (date == null) {
-			return null;
-		}
-		return DEFAULT_DATE_FORMATER.format(date);
-	}
+    public final static String FORMAT_YYYY = "yyyy";
 
-	public static String formatTime(Date date) {
-		if (date == null) {
-			return null;
-		}
-		return DEFAULT_TIME_FORMATER.format(date);
-	}
+    public final static String FORMAT_YYYYMM = "yyyyMM";
 
-	public static String getHumanDisplayForTimediff(Long diffMillis) {
-		if (diffMillis == null) {
-			return "";
-		}
-		long day = diffMillis / (24 * 60 * 60 * 1000);
-		long hour = (diffMillis / (60 * 60 * 1000) - day * 24);
-		long min = ((diffMillis / (60 * 1000)) - day * 24 * 60 - hour * 60);
-		long se = (diffMillis / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
-		StringBuilder sb = new StringBuilder();
-		if (day > 0) {
-			sb.append(day + "D");
-		}
-		DecimalFormat df = new DecimalFormat("00");
-		sb.append(df.format(hour) + ":");
-		sb.append(df.format(min) + ":");
-		sb.append(df.format(se));
-		return sb.toString();
-	}
+    public final static String FORMAT_YYYYMMDD = "yyyyMMdd";
+
+    public final static DateFormat FORMAT_YYYY_FORMATER = new SimpleDateFormat(FORMAT_YYYY);
+
+    public final static DateFormat FORMAT_YYYYMM_FORMATER = new SimpleDateFormat(FORMAT_YYYYMM);
+
+    public final static DateFormat FORMAT_YYYYMMDD_FORMATER = new SimpleDateFormat(FORMAT_YYYYMMDD);
+
+    public static String formatDate(Date date) {
+        if (date == null) {
+            return null;
+        }
+        return DEFAULT_DATE_FORMATER.format(date);
+    }
+
+    public static String formatTime(Date date) {
+        if (date == null) {
+            return null;
+        }
+        return DEFAULT_TIME_FORMATER.format(date);
+    }
+
+    public static Date parseDate(String date, DateFormat df) {
+        if (date == null) {
+            return null;
+        }
+        try {
+            return df.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Date parseTime(String date, DateFormat df) {
+        if (date == null) {
+            return null;
+        }
+        try {
+            return df.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Date parseDate(String date) {
+        return parseDate(date, DEFAULT_DATE_FORMATER);
+    }
+
+    public static Date parseTime(String date) {
+        return parseTime(date, DEFAULT_TIME_FORMATER);
+    }
+
+    public static String plusOneDay(String date) {
+        DateTime dateTime = new DateTime(parseDate(date).getTime());
+        return formatDate(dateTime.plusDays(1).toDate());
+    }
+
+    public static String getHumanDisplayForTimediff(Long diffMillis) {
+        if (diffMillis == null) {
+            return "";
+        }
+        long day = diffMillis / (24 * 60 * 60 * 1000);
+        long hour = (diffMillis / (60 * 60 * 1000) - day * 24);
+        long min = ((diffMillis / (60 * 1000)) - day * 24 * 60 - hour * 60);
+        long se = (diffMillis / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+        StringBuilder sb = new StringBuilder();
+        if (day > 0) {
+            sb.append(day + "D");
+        }
+        DecimalFormat df = new DecimalFormat("00");
+        sb.append(df.format(hour) + ":");
+        sb.append(df.format(min) + ":");
+        sb.append(df.format(se));
+        return sb.toString();
+    }
 }
