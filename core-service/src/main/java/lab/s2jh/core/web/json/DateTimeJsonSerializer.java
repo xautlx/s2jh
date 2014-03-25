bf0c89e884445f16d93492bf837671716640041c
xautlx <xautlx@hotmail.com>
2014-03-25 09:00:37 +0800

    调整几个Jackson序列表帮助类
    Signed-off-by: xautlx <xautlx@hotmail.com>

--- core-service/src/main/java/lab/s2jh/core/web/json/DateTimeJsonSerializer.java
+++ core-service/src/main/java/lab/s2jh/core/web/json/DateTimeJsonSerializer.java
@@ -1,9 +1,9 @@
 package lab.s2jh.core.web.json;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import lab.s2jh.core.util.DateUtils;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -12,15 +12,12 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 public class DateTimeJsonSerializer extends JsonSerializer<Date> {
 
-    public static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
     @Override
     public void serialize(Date value, JsonGenerator jgen, SerializerProvider provider) throws IOException,
             JsonProcessingException {
         if (value != null) {
-            jgen.writeString(DATE_FORMAT.format(value));
+            jgen.writeString(DateUtils.formatTime(value));
         }
-
     }
 
     public Class<Date> handledType() {
