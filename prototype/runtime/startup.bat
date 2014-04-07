6faa42f164548e0bc85e622d56da5d188aec3442
xautlx <xautlx@hotmail.com>
2014-04-07 12:33:09 +0800

    优化样例运行启动脚本

--- prototype/runtime/startup.bat
+++ prototype/runtime/startup.bat
@@ -1,13 +1,14 @@
 @echo off
 set PORT=8080
 set CONTEXT=prototype
-set WAR=prototype-1.0.0-simplified.war
+set WAR=prototype-simplified.war
 set JAVA_OPT=-Xms256m -Xmx1024m -XX:MaxPermSize=128m -Dport=%PORT% -Dcontext=%CONTEXT%
 if not exist h2 (set JAVA_OPT=%JAVA_OPT% -Djdbc.initialize.database.enable=true) else (set JAVA_OPT=%JAVA_OPT% -Djdbc.initialize.database.enable=false)
+
+if not exist work (
 echo ---------------------------------------------------------------
-echo Prepare to clean temp work dir and restore files...
+echo Prepare to init work files...
 echo ---------------------------------------------------------------
-rm -fr work
 mkdir work\webapp
 copy %WAR%  work\webapp
 cd  work\webapp\
@@ -15,6 +16,17 @@ jar xvf %WAR%
 cd ..\..
 rm work\webapp\%WAR%
 copy lib\* work\webapp\WEB-INF\lib
+) else (
+echo ---------------------------------------------------------------
+echo Prepare to update work files...
+echo ---------------------------------------------------------------
+copy %WAR%  work\webapp
+cd  work\webapp\
+jar xvf %WAR% biz common pub resources WEB-INF
+cd ..\..
+rm work\webapp\%WAR%
+)
+
 echo ---------------------------------------------------------------
 echo [INFO] JAVA_OPT=%JAVA_OPT%
 echo [INFO] Please wait a moment for startup finish, when you see:
