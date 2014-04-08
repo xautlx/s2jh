c21ac7464a515941fb965439c33caea34861ee6e
xautlx <xautlx@hotmail.com>
2014-04-08 22:26:02 +0800

    优化独立运行程序，减少单个增量更新文件大小

--- prototype/runtime/startup.bat
+++ prototype/runtime/startup.bat
@@ -1,31 +1,30 @@
 @echo off
 set PORT=8080
 set CONTEXT=prototype
+set JETTY=prototype-jetty.jar
 set WAR=prototype-simplified.war
 set JAVA_OPT=-Xms256m -Xmx1024m -XX:MaxPermSize=128m -Dport=%PORT% -Dcontext=%CONTEXT%
-if not exist h2 (set JAVA_OPT=%JAVA_OPT% -Djdbc.initialize.database.enable=true) else (set JAVA_OPT=%JAVA_OPT% -Djdbc.initialize.database.enable=false)
+
+echo ---------------------------------------------------------------
+echo Preparing work files...
+echo ---------------------------------------------------------------
+
+if not exist h2 (
+set JAVA_OPT=%JAVA_OPT% -Djdbc.initialize.database.enable=true
+) else (
+set JAVA_OPT=%JAVA_OPT% -Djdbc.initialize.database.enable=false
+)
 
 if not exist work (
-echo ---------------------------------------------------------------
-echo Prepare to init work files...
-echo ---------------------------------------------------------------
-mkdir work\webapp
+mkdir work\webapp\WEB-INF\lib
+copy lib\* work\webapp\WEB-INF\lib
+) 
+
 copy %WAR%  work\webapp
 cd  work\webapp\
 jar xvf %WAR%
 cd ..\..
 rm work\webapp\%WAR%
-copy lib\* work\webapp\WEB-INF\lib
-) else (
-echo ---------------------------------------------------------------
-echo Prepare to update work files...
-echo ---------------------------------------------------------------
-copy %WAR%  work\webapp
-cd  work\webapp\
-jar xvf %WAR% biz common pub resources WEB-INF
-cd ..\..
-rm work\webapp\%WAR%
-)
 
 echo ---------------------------------------------------------------
 echo [INFO] JAVA_OPT=%JAVA_OPT%
