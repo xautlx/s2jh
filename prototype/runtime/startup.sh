5fe52e0ee3939bbefc25d9f9f8cb06d2d394e4ce
lixia <xautlx@hotmail.com>
2014-04-26 18:47:02 +0800

    linux版本prototype启动脚本
    Signed-off-by: lixia <xautlx@hotmail.com>

---
+++ prototype/runtime/startup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PORT="80"
+CONTEXT="prototype"
+JETTY="prototype-jetty.jar"
+WAR="prototype-simplified.war"
+JAVA_OPT="-Xms256m -Xmx512m -XX:MaxPermSize=128m -Dport=$PORT -Dcontext=$CONTEXT"
+
+echo "---------------------------------------------------------------"
+echo "Preparing work files..."
+echo "---------------------------------------------------------------"
+
+if [ -d "./h2" ]
+then
+JAVA_OPT="$JAVA_OPT -Djdbc.initialize.database.enable=false"
+else
+JAVA_OPT="$JAVA_OPT -Djdbc.initialize.database.enable=true"
+fi
+
+if [ -d "./work" ]
+then
+echo "work dir exist, do nothing"
+else
+mkdir -p  work/webapp/WEB-INF/lib
+cp lib/* work/webapp/WEB-INF/lib
+fi
+
+cp $WAR  work/webapp
+cd  work/webapp/
+jar xvf $WAR
+cd ../..
+rm work/webapp/$WAR
+
+echo "---------------------------------------------------------------"
+echo "[INFO] JAVA_OPT=$JAVA_OPT"
+echo "[INFO] Please wait a moment for startup finish, when you see:"
+echo "[INFO]   ...Started SelectChannelConnector@0.0.0.0:$PORT..."
+echo "[INFO] then use Firefox to visit the following URL:"
+echo "[INFO] http://localhost:$PORT/$CONTEXT"
+echo "---------------------------------------------------------------"
+
+java $JAVA_OPT -jar $WAR
