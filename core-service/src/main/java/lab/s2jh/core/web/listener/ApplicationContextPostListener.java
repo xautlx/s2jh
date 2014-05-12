d9f8f8eb5d32704d191557dc16ebb34eb36bdeba
lixia <xautlx@hotmail.com>
2014-05-12 14:14:56 +0800

    * 复制实现Apache Commons Lang的ClassUtils对应接口实现      * 主要是为了解决把应用的jar文件以tomcat\lib共享模式部署类加载机制访问问题
    Signed-off-by: lixia <xautlx@hotmail.com>

--- core-service/src/main/java/lab/s2jh/core/web/listener/ApplicationContextPostListener.java
+++ core-service/src/main/java/lab/s2jh/core/web/listener/ApplicationContextPostListener.java
@@ -2,6 +2,8 @@ package lab.s2jh.core.web.listener;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -13,7 +15,6 @@ import lab.s2jh.core.annotation.MetaData;
 import lab.s2jh.core.context.SpringContextHolder;
 import lab.s2jh.core.dao.jpa.ExtPersistenceUnitPostProcessor;
 
-import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -54,8 +55,8 @@ public class ApplicationContextPostListener implements ServletContextListener {
             MutablePersistenceUnitInfo pui = persistenceUnitPostProcessor.getMutablePersistenceUnitInfo();
 
             //循环所有Entity对象中的Enum定义，转换成对应的Map结构数据存入ServletContext属性中，便于Web层标签直接方便获取
-            for (Class<?> entityClass : ClassUtils.convertClassNamesToClasses(pui.getManagedClassNames())) {
-                //logger.debug("Post process {}", entityClass);
+            for (Class<?> entityClass : convertClassNamesToClasses(pui.getManagedClassNames())) {
+                logger.trace("Post processing entity: {}", entityClass);
                 Field[] fields = entityClass.getDeclaredFields();
                 for (Field field : fields) {
                     Class fieldClass = field.getType();
@@ -114,4 +115,24 @@ public class ApplicationContextPostListener implements ServletContextListener {
         logger.debug("Invoke ApplicationContextPostListener contextDestroyed");
     }
 
+    /**
+     * 复制实现Apache Commons Lang的ClassUtils对应接口实现
+     * 主要是为了解决把应用的jar文件以tomcat\lib共享模式部署类加载机制访问问题
+     * @param classNames
+     * @return
+     */
+    private List<Class<?>> convertClassNamesToClasses(List<String> classNames) {
+        if (classNames == null) {
+            return null;
+        }
+        List<Class<?>> classes = new ArrayList<Class<?>>(classNames.size());
+        for (String className : classNames) {
+            try {
+                classes.add(Class.forName(className));
+            } catch (Exception ex) {
+                classes.add(null);
+            }
+        }
+        return classes;
+    }
 }
