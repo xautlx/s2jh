290dd5b907e4deb6f3734dbc47e5b5c0501b0ed1
lixia <xautlx@hotmail.com>
2014-05-21 13:33:25 +0800

    修正AuthContextHolder多线程处理问题
    Signed-off-by: lixia <xautlx@hotmail.com>

--- core-service/src/main/java/lab/s2jh/core/security/AuthContextHolder.java
+++ core-service/src/main/java/lab/s2jh/core/security/AuthContextHolder.java
@@ -4,7 +4,6 @@
 package lab.s2jh.core.security;
 
 import java.util.Collection;
-import java.util.Locale;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,55 +17,36 @@ public class AuthContextHolder {
 
     public static final String SPRING_SECURITY_LAST_USERNAME_KEY = "SPRING_SECURITY_LAST_USERNAME";
 
-    private static final ThreadLocal<String> userPinContainer = new ThreadLocal<String>();
-
-    private static final ThreadLocal<Locale> localeContainer = new ThreadLocal<Locale>();
-
     public static final String DEFAULT_UNKNOWN_PIN = "N/A";
-
-    public static final Locale DEFAULT_LOCALE = new Locale("zh", "cn");
-
-    public static void setAuthUserPin(String userPin) {
-        userPinContainer.set(userPin);
-    }
 
     /**
      * 获取用户唯一登录标识
      */
     public static String getAuthUserPin() {
-        synchronized (userPinContainer) {
-            String pin = userPinContainer.get();
-            if (StringUtils.isBlank(pin)) {
-                AuthUserDetails authUserDetails = getAuthUserDetails();
-                if (authUserDetails != null && authUserDetails.getUsername() != null) {
-                    pin = authUserDetails.getUsername();
-                } else {
-                    pin = DEFAULT_UNKNOWN_PIN;
-                }
-                userPinContainer.set(pin);
-            }
-            return pin;
+        String pin = DEFAULT_UNKNOWN_PIN;
+        AuthUserDetails authUserDetails = getAuthUserDetails();
+        if (authUserDetails != null && authUserDetails.getUsername() != null) {
+            pin = authUserDetails.getUsername();
         }
+        return pin;
     }
 
     /**
      * 基于Spring Security获取用户认证信息
      */
     public static AuthUserDetails getAuthUserDetails() {
-        synchronized (userPinContainer) {
-            AuthUserDetails userDetails = null;
-            if (SecurityContextHolder.getContext() == null
-                    || SecurityContextHolder.getContext().getAuthentication() == null
-                    || SecurityContextHolder.getContext().getAuthentication().getPrincipal() == null) {
-                return null;
-            }
-            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            if (principal instanceof AuthUserDetails) {
-                userDetails = (AuthUserDetails) principal;
-            }
-            Assert.notNull(userDetails);
-            return userDetails;
+        AuthUserDetails userDetails = null;
+        if (SecurityContextHolder.getContext() == null
+                || SecurityContextHolder.getContext().getAuthentication() == null
+                || SecurityContextHolder.getContext().getAuthentication().getPrincipal() == null) {
+            return null;
         }
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof AuthUserDetails) {
+            userDetails = (AuthUserDetails) principal;
+        }
+        Assert.notNull(userDetails);
+        return userDetails;
     }
 
     /**
@@ -98,16 +78,5 @@ public class AuthContextHolder {
 
     public static Collection<String> getAclCodePrefixs() {
         return getAuthUserDetails().getAclCodePrefixs();
-    }
-
-    /**
-     * 获取Locale
-     */
-    public static Locale getLocale() {
-        Locale locale = localeContainer.get();
-        if (locale == null) {
-            return DEFAULT_LOCALE;
-        }
-        return locale;
     }
 }
