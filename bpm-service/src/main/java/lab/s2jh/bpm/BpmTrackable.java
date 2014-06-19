7d16b7966e147d9cc1931d38349546eb71692542
lixia <xautlx@hotmail.com>
2014-06-19 15:14:09 +0800

    /**  * 标识实体会进行BPM流程处理过程回调，写入当前业务对象所处的工作流节点  * 一般在实体定义private String activeTaskName属性，然后生成对应的setter和getter方式即可  */ public interface BpmTrackable
    Signed-off-by: lixia <xautlx@hotmail.com>

---
+++ bpm-service/src/main/java/lab/s2jh/bpm/BpmTrackable.java
@@ -0,0 +1,25 @@
+package lab.s2jh.bpm;
+
+import javax.persistence.Transient;
+
+import lab.s2jh.core.entity.annotation.SkipParamBind;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * 标识实体会进行BPM流程处理过程回调，写入当前业务对象所处的工作流节点
+ * 一般在实体定义private String activeTaskName属性，然后生成对应的setter和getter方式即可
+ */
+public interface BpmTrackable {
+
+    @Transient
+    @JsonIgnore
+    public String getBpmBusinessKey();
+
+    @JsonProperty
+    public String getActiveTaskName();
+
+    @SkipParamBind
+    public void setActiveTaskName(String activeTaskName);
+}
