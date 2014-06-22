848f6d23d5f2a38dc77cac85e54f3ff4388d1487
lixia <xautlx@hotmail.com>
2014-06-22 16:18:40 +0800

    基础数据sql脚本添加数据
    Signed-off-by: lixia <xautlx@hotmail.com>

--- prototype/src/main/resources/sql/data/t_auth_user.sql
+++ prototype/src/main/resources/sql/data/t_auth_user.sql
@@ -1,1 +1,1 @@
-INSERT INTO t_auth_user (sid,  version, user_id, password, nick, enabled ,uid) VALUES (1,  0, 'admin', 'ceb4f32325eda6142bd65215f4c0f371', '系统超级管理员', 'Y', 'admin');
+INSERT INTO t_auth_user (sid,  version, user_id, password, nick, enabled ,uid,  DEPARTMENT_ID) VALUES (1,  0, 'admin', 'ceb4f32325eda6142bd65215f4c0f371', '系统超级管理员', 'Y', 'admin','1cc7a4f1-44ee-41e2-b488-f0769fa6eca6');
--- prototype/src/main/resources/sql/data/t_sys_data_dict.sql
+++ prototype/src/main/resources/sql/data/t_sys_data_dict.sql
@@ -2,3 +2,8 @@ INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled
 INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled, order_rank, parent_id) VALUES ('402880c53e10e5a0013e10e6dd251000', 1, 'MENU', '菜单', 0, 500, '402880c53e10e5a0013e10e6dd250009');
 INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled, order_rank, parent_id) VALUES ('402880c53e10e5a0013e10e6dd251001', 1, 'URL', 'URL', 0, 400, '402880c53e10e5a0013e10e6dd250009');
 INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled, order_rank, parent_id) VALUES ('402880c53e10e5a0013e10e6dd251002', 1, 'BTN', '按钮', 0, 300, '402880c53e10e5a0013e10e6dd250009');
+
+INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled, order_rank, parent_id) VALUES ('402880c53e10e5a0013e10e6dd251003', 0, 'BIZ_SALE_DELIVERY_ORDER_SOURCE', '销售单来源', 0, 100, null);
+INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled, order_rank, parent_id) VALUES ('402880c53e10e5a0013e10e6dd251004', 0, 'JD', '京东', 0, 500, '402880c53e10e5a0013e10e6dd251003');
+INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled, order_rank, parent_id) VALUES ('402880c53e10e5a0013e10e6dd251005', 0, 'TB', '淘宝', 0, 400, '402880c53e10e5a0013e10e6dd251003');
+INSERT INTO tbl_sys_data_dict (id, version, PRIMARY_KEY, PRIMARY_VALUE, disabled, order_rank, parent_id) VALUES ('402880c53e10e5a0013e10e6dd251006', 0, 'TM', '天猫', 0, 300, '402880c53e10e5a0013e10e6dd251003');
