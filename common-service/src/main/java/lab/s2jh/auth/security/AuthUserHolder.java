e213bb439643bec13b8c09633bdb32846db3603f
lixia <xautlx@hotmail.com>
2014-07-10 14:24:17 +0800

    获取当前登录用户所属部门对象public static Department getLogonUserDepartment()
    Signed-off-by: lixia <xautlx@hotmail.com>

--- common-service/src/main/java/lab/s2jh/auth/security/AuthUserHolder.java
+++ common-service/src/main/java/lab/s2jh/auth/security/AuthUserHolder.java
@@ -1,6 +1,7 @@
 package lab.s2jh.auth.security;
 
 import lab.s2jh.auth.dao.UserDao;
+import lab.s2jh.auth.entity.Department;
 import lab.s2jh.auth.entity.User;
 import lab.s2jh.core.context.SpringContextHolder;
 import lab.s2jh.core.security.AuthContextHolder;
@@ -11,8 +12,22 @@ import lab.s2jh.core.security.AuthContextHolder;
  */
 public class AuthUserHolder {
 
+    /**
+     * 获取当前登录用户对象
+     */
     public static User getLogonUser() {
         UserDao userDao = SpringContextHolder.getBean(UserDao.class);
         return userDao.findByUid(AuthContextHolder.getAuthUserDetails().getUid());
+    }
+
+    /**
+     * 获取当前登录用户所属部门对象
+     */
+    public static Department getLogonUserDepartment() {
+        User user = getLogonUser();
+        if (user == null) {
+            return null;
+        }
+        return user.getDepartment();
     }
 }
