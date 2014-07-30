5e0b9ce03d8131620d269f56f5876b93927a3968
lixia <xautlx@hotmail.com>
2014-07-30 09:38:13 +0800

    移除bpm-service工程
    Signed-off-by: lixia <xautlx@hotmail.com>

--- bpm-service/src/main/java/lab/s2jh/bpm/web/action/package-info.java
+++
@@ -1,7 +0,0 @@
-@Namespace("/bpm")
-@MetaData(value = "工作流模块")
-package lab.s2jh.bpm.web.action;
-import lab.s2jh.core.annotation.MetaData;
-
-import org.apache.struts2.convention.annotation.Namespace;
-
